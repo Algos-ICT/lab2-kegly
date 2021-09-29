0f49eecf66ba08ab5286c69ad68b044cfca28f7e
kegly <90460093+kegly@users.noreply.github.com>
2021-09-30 00:26:38 +0300

    Update task5.py

--- исходный код/task5.py
+++ исходный код/task5.py
@@ -2,12 +2,6 @@ file=open('input.txt')
 line=file.readline()
 A=list(map(int,line.split()))
 
-#temp=list()
-#for i in range(len(A)):
-#    temp+=[min(A)]
-#    A.remove(min(A))
-#print(temp)
-
 for i in range(len(A)-1):
     for k in range(i+1, len(A)):
         if A[k]<A[i]:
