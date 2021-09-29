8bbef80c3965259ce0538166a967411abd1cab0a
kegly <90460093+kegly@users.noreply.github.com>
2021-09-30 00:53:11 +0300

    Update task9.py

--- исходный код/task9.py
+++ исходный код/task9.py
@@ -1,17 +1,18 @@
 f=open('input.txt')
-line1=f.readline()
-line2=f.readline()
-arr1=list(map(int, line1.split()))
-arr2=list(map(int, line2.split()))
+a, b = input().split() # считали два двоичных числа
+arr1=[]+a # занесли первое число в n-битовый массив
+arr2=[]+b # анесли второе число в n-битовый массив
 v1=''
 v2=''
-for i in arr1:
-    v1+=str(arr1[i])
+for i in arr1:  # обратно создаем 2 строки с двочными числами
+    v1+=arr1[i]
 for i in arr2:
-    v2+=str(arr2[i])
+    v2+=arr2[i]
 sum=int(v1,2)+int(v2,2)
-sum=bin(sum)
-sum=sum[2:len(sum)]
+sum=bin(sum) # перевели сумму в двоичную СС
+sum=sum[2:len(sum)] #убрали префикс 0b с помощью среза
 l=list()
-l+=sum
-print(l)
+l+=sum #занесли сумму в (n-1)-элементный масссив
+file=open('output.txt', 'w')
+file.write(sum)
+file.close()
